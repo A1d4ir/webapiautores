3c1e6dd658f6de7f2904ebe8af6e7f4bb6ffaf57
ProdynamicsDevEcom <devecom@prodynamics.com.mx>
2023-03-03 11:29:26 -0600

    Prueba inicial

---
+++ WebApiAutores/DTOs/AutorDTO.cs
@@ -0,0 +1,10 @@
+﻿using WebApiAutores.Entidades;
+
+namespace WebApiAutores.DTOs
+{
+    public class AutorDTO: Recurso
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
